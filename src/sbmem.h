92307a7c44ade3c4ed10080e487cfcf12aa41d4c
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-04-20 23:58:39 +0300

    changed files structure + added problem statement

--- src/sbmem.h
+++ src/sbmem.h
@@ -1,6 +1,3 @@
-
-
-
 int sbmem_init(int segmentsize); 
 int sbmem_remove(); 
 
@@ -8,8 +5,3 @@ int sbmem_open();
 void *sbmem_alloc (int size);
 void sbmem_free(void *p);
 int sbmem_close(); 
-
-
-
-    
-    
