9d7a135cdaf7282f91ac5e38f0ae8c022f7895eb
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-04-21 00:44:10 +0300

    implemented memory initialization

--- src/sbmemlib.c
+++ src/sbmemlib.c
@@ -1,8 +1,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h> 
 
 // Define a name for your shared memory; you can give any name that start with a slash character; it will be like a filename.
+#define MBMEM_NAME "/sbmemlib"
 
 // Define semaphore(s)
 
@@ -10,8 +13,15 @@
 
 int sbmem_init(int segmentsize)
 {
-    printf ("sbmem init called"); // remove all printfs when you are submitting to us.  
-    return (0); 
+    int res = shm_open(MBMEM_NAME, O_RDWR | O_CREAT | O_TRUNC,  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    
+    if (res == -1) {
+        printf("An error occured while creating shared memory");
+        return(1);
+    } else {
+        printf ("sbmem init called"); // remove all printfs when you are submitting to us.  
+        return (0); 
+    }
 }
 
 int sbmem_remove()
